7fc5f38e2156201580bab15b22fa581b42f5da91
KaiGai Kohei <kaigai@ak.jp.nec.com>
2007-11-16 20:54:06 -0800

    The following patch to libcap enables to display file capabilities recursively on the enumerated directories when -r is specified.
    In addition, some other features are ported from my getfcap.
    When an entry contains no file-capabilities, displaying it will be
    skipped without returning an error. However, -v option enables to
    display those filenames with no capabilities.
    -h options displays short usage message.
    
    Please consider to apply it on your tree.
    
    EXAMPLE:
      [kaigai@saba libcap]$ ./progs/getcap -r /tmp
      /tmp/ping = cap_net_raw+ep
      [kaigai@saba libcap]$
    
    Thanks,
    --
    OSS Platform Development Division, NEC
    KaiGai Kohei <kaigai@ak.jp.nec.com>

--- progs/getcap.c
+++ progs/getcap.c
@@ -8,45 +8,112 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <sys/capability.h>
+
+static int verbose = 0;
+static int recursive = 0;
 
 static void usage(void)
 {
     fprintf(stderr,
-	    "usage: getcap <filename> [<filename> ...]\n"
+	    "usage: getcap [-v] [-r] [-h] <filename> [<filename> ...]\n"
 	    "\n"
 	    "\tdisplays the capabilities on the queried file(s).\n"
 	);
     exit(1);
 }
 
+static void do_recursive(const char *fname);
+
+static void do_getcap(const char *fname)
+{
+    cap_t cap_d;
+    char *result;
+
+    cap_d = cap_get_file(fname);
+    if (cap_d == NULL) {
+	if (errno != ENODATA) {
+	    fprintf(stderr, "Failed to get capabilities of file `%s' (%s)\n",
+		    fname, strerror(errno));
+	} else if (verbose) {
+	    printf("%s\n", fname);
+	}
+	goto out;
+    }
+
+    result = cap_to_text(cap_d, NULL);
+    if (!result) {
+	fprintf(stderr,
+		"Failed to get capabilities of human readable format at `%s' (%s)\n",
+		fname, strerror(errno));
+	cap_free(cap_d);
+	return;
+    }
+    printf("%s %s\n", fname, result);
+    cap_free(cap_d);
+    cap_free(result);
+
+  out:
+    if (recursive) {
+	struct stat stbuf;
+
+	if (stat(fname, &stbuf)) {
+	    fprintf(stderr, "Failed to get attribute of file `%s' (%s)\n",
+		    fname, strerror(errno));
+	} else if (S_ISDIR(stbuf.st_mode)) {
+	    do_recursive(fname);
+	}
+    }
+}
+
+static void do_recursive(const char *fname)
+{
+    DIR *dirp;
+    struct dirent *dent;
+    char buffer[PATH_MAX];
+
+    dirp = opendir(fname);
+    if (dirp == NULL) {
+	fprintf(stderr, "Failed to open directory `%s' (%s)\n",
+		fname, strerror(errno));
+	return;
+    }
+
+    while ((dent = readdir(dirp)) != NULL) {
+	if (!strcmp(dent->d_name, ".") || !strcmp(dent->d_name, ".."))
+	    continue;
+	snprintf(buffer, PATH_MAX, "%s/%s", fname, dent->d_name);
+	do_getcap(buffer);
+    }
+    closedir(dirp);
+}
+
 int main(int argc, char **argv)
 {
-    char *result=NULL;
+    int i, c;
 
-    if (argc < 2) {
-	usage();
-    }
-
-    for ( ++argv; --argc > 0; ++argv ) {
-	ssize_t length;
-	cap_t cap_d;
-
-	cap_d = cap_get_file(argv[0]);
-
-	if (cap_d == NULL) {
-	    fprintf(stderr, "Failed to get capabilities for file `%s' (%s)\n",
-		    argv[0], strerror(errno));
-	    continue;
+    while ((c = getopt(argc, argv, "rvh")) > 0) {
+	switch(c) {
+	case 'r':
+	    recursive = 1;
+	    break;
+	case 'v':
+	    verbose = 1;
+	    break;
+	default:
+	    usage();
 	}
-
-	result = cap_to_text(cap_d, &length);
-
-	printf("%s: %s\n", *argv, result);
-
-	cap_free(result);
-	cap_free(cap_d);
     }
+
+    if (!argv[optind])
+	usage();
+
+    for (i=optind; argv[i] != NULL; i++)
+	do_getcap(argv[i]);
 
     return 0;
 }
